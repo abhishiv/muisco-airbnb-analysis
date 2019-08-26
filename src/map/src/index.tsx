d0370c6188e1e5ba73d769b1faa6d024f0fd34ed
Abhishiv Saxena <abhishiv@gmail.com>
2019-08-26 22:48:23 +0200

    should build

--- src/map/src/index.tsx
+++ src/map/src/index.tsx
@@ -1,15 +1,1 @@
-import * as React from "react";
-import { render } from "react-dom";
 
-import WorldMap from "./map";
-
-function App() {
-  return (
-    <div >
-      <WorldMap />
-    </div>
-  );
-}
-
-const rootElement = document.getElementById("root");
-render(<App />, rootElement);
