7f0777255661f8c88147f6f9bae4d9f9a8f80b21
Abhishiv Saxena <abhishiv@gmail.com>
2019-08-28 19:11:09 +0200

    toggle

--- src/map/src/tiles.tsx
+++ src/map/src/tiles.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { tile } from "d3-tile";
+import { geoPath } from "d3-geo";
+import { feature } from "topojson-client";
 import world from "./110m.json";
+import w from "./world-countries.json";
 import us from "./us.json";
 us;
 world;
@@ -8,22 +11,25 @@ export interface TilesProps {
   width: number;
   height: number;
 }
-function position(tile: any, tiles: any) {
-  const [x, y] = tile;
-  const {
-    translate: [tx, ty],
-    scale: k
-  } = tiles;
-  return [(x + tx) * k, (y + ty) * k];
-}
 export interface TilesState {
   tiles: any[];
+  projection: any;
+  features: any;
+  location: any;
 }
 import { geoMercator } from "d3-geo";
+function floor(k: number) {
+  return Math.pow(2, Math.floor(Math.log(k) / Math.LN2));
+}
 class Tiles extends React.Component<TilesProps, TilesState> {
   constructor(props: TilesProps) {
     super(props);
-    this.state = { tiles: [] };
+    this.state = {
+      tiles: [],
+      projection: null,
+      features: null,
+      location: null
+    };
   }
   componentDidMount() {
     const { width, height } = this.props;
@@ -34,39 +40,44 @@ class Tiles extends React.Component<TilesProps, TilesState> {
       .scale(1 / tau)
       .translate([0, 0]);
 
-    const bounds = [[70, 20], [90, 40]];
+    const center = [80, 25];
+    const delta = 1;
+    const bounds = [
+      [center[0] - delta, center[1] - delta],
+      [center[0], center[1]]
+    ];
     const p0 = projection([bounds[0][0], bounds[1][1]]);
     const p1 = projection([bounds[1][0], bounds[0][1]]);
-
-    function floor(k) {
-      return Math.pow(2, Math.floor(Math.log(k) / Math.LN2));
+    let k: number = 1;
+    let tx: number = 0;
+    let ty: number = 0;
+    if (p1 && p0) {
+      k = floor(
+        0.95 / Math.max((p1[0] - p0[0]) / width, (p1[1] - p0[1]) / height)
+      );
+      tx = (width - k * (p1[0] + p0[0])) / 2;
+      ty = (height - k * (p1[1] + p0[1])) / 2;
+      console.log("k", k, tau, k / tau);
+      projection.scale(k / tau).translate([tx, ty]);
     }
-
-    const k = floor(
-      0.95 / Math.max((p1[0] - p0[0]) / width, (p1[1] - p0[1]) / height)
-    );
-    const tx = (width - k * (p1[0] + p0[0])) / 2;
-    const ty = (height - k * (p1[1] + p0[1])) / 2;
-
-    projection.scale(k / tau).translate([tx, ty]);
 
     const tiles = tile()
       .size([width, height])
       .scale(k)
       .tileSize(256)
       .translate([tx, ty])();
+    const features: any = feature(
+      (w as unknown) as any,
+      w.objects.countries1 as any
+    );
 
     console.log("tiles", tiles);
-    this.setState({ tiles });
-    for (const t of tiles) {
-      console.log(`tile ${t} is at ${position(t, tiles)}`);
-    }
+    this.setState({ tiles, projection, features });
   }
   renderTiles = (tiles: any) => {
     return (
-      <React.Fragment>
+      <div>
         {tiles.map((tile: any, i: number) => {
-          const p = position(tile, tiles);
           //console.log("tile", tile, p);
           const style = {
             position: "absolute",
@@ -91,13 +102,42 @@ class Tiles extends React.Component<TilesProps, TilesState> {
                 backgroundSize: "cover",
                 width: 256,
                 height: 256,
-                ...style
+                ...(style as any)
               }}
               key={i}
             ></div>
           );
         })}
-      </React.Fragment>
+      </div>
+    );
+  };
+  renderSVG = () => {
+    const { width, height } = this.props;
+    const p = geoPath(this.state.projection);
+    console.log("world", w, this.state);
+    return (
+      <svg
+        style={{ position: "absolute" }}
+        width={width}
+        height={height}
+        viewBox={`0 0 ${width} ${height}`}
+      >
+        <g className="counties">
+          {this.state.features.features.map((d: any, i: number) => {
+            return (
+              <path
+                key={i}
+                d={p(d) as any}
+                className="country"
+                onClick={() => {}}
+                fill={`rgba(38,50,56,${0})`}
+                stroke="#000"
+                strokeWidth={0.5}
+              />
+            );
+          })}
+        </g>
+      </svg>
     );
   };
   render() {
@@ -111,6 +151,7 @@ class Tiles extends React.Component<TilesProps, TilesState> {
         }}
       >
         {this.state.tiles && this.renderTiles(this.state.tiles)}
+        {this.state.features && this.renderSVG()}
       </div>
     );
   }
