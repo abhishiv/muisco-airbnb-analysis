74bf082beb125918ca8461c5db5b221be7e92b6f
Abhishiv Saxena <abhishiv@gmail.com>
2019-08-29 22:50:29 +0200

    vector tiles

--- src/hub/src/kernel.ts
+++ src/hub/src/kernel.ts
@@ -2,7 +2,7 @@ import parse from "csv-parse";
 
 export function parseCSV(text: string) {
   return new Promise(function(resolve, reject) {
-    const output = [];
+    const output: any[] = [];
     parse(text, {
       trim: true,
       skip_empty_lines: true
@@ -21,6 +21,39 @@ export function parseCSV(text: string) {
       .on("error", (e: any) => reject(e));
   });
 }
+declare var lf: any;
+export async function loadIntoDB(list: any[], headers: any[]): Promise<any[]> {
+  var schemaBuilder = lf.schema.create("items", 1);
+
+  schemaBuilder
+    .createTable("reviews")
+    .addColumn("id", lf.Type.STRING)
+    .addColumn("listing_id", lf.Type.INTEGER)
+    .addColumn("date", lf.Type.DATE_TIME)
+    .addPrimaryKey(["id"])
+    .addIndex("idxListing", ["listing_id"], false, lf.Order.DESC)
+    .addIndex("idxDate", ["date"], false, lf.Order.DESC);
+
+  const db = await schemaBuilder.connect();
+  const item = db.getSchema().table("reviews");
+  const rows = list.map(el => {
+    return item.createRow({
+      id: el[0] + el[1],
+      listing_id: el[0],
+      date: el[1]
+    });
+  });
+  await db
+    .insertOrReplace()
+    .into(item)
+    .values(rows)
+    .exec();
+  const res = await db
+    .select(item.id)
+    .from(item)
+    .exec();
+  return res;
+}
 
 export default async function boot() {
   console.time("donwload_csv");
@@ -30,7 +63,13 @@ export default async function boot() {
   const text = await fileReq.text();
   console.timeEnd("donwload_csv");
   console.time("parse_csv");
-  const parsed = await parseCSV(text);
+  const parsed: any[] = await parseCSV(text);
   console.timeEnd("parse_csv");
   console.log("json.", parsed);
+  console.time("insert_csv");
+  const header = parsed.slice(0, 1)[0];
+  const list = parsed.slice(1, parsed.length);
+  const res = await loadIntoDB(list, header);
+  console.timeEnd("insert_csv");
+  console.log("res", res);
 }
